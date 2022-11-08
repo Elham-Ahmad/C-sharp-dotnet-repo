5d494f300c3a60efa676244112b2555dc9b40e2a
Elham Ahmad <Elham.Ahmad@emtecinc.com>
2022-11-08 12:19:15 +0530

    Initial commit in feature one branch

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hello world, C#");
+        }
+    }
+}
