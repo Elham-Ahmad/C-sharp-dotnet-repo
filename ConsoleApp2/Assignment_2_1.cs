f0bec5cb86542317653b91b513fb740604d1f5b4
Elham Ahmad <Elham.Ahmad@emtecinc.com>
2022-11-11 19:36:45 +0530

    First Commit with C# assignment-2 on (11/11/22)

---
+++ ConsoleApp2/Assignment_2_1.cs
@@ -0,0 +1,30 @@
+﻿//  1.Write a C# program to validate an Email ID using – 
+//  a.	Using Regex
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Assignment_2_1
+    {
+        public void assignment2_1()
+        {
+            string email = "";
+            Console.WriteLine($"Enter your Email");
+            email  = Console.ReadLine();
+
+            string pattern = @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z";
+            if (Regex.IsMatch(email, pattern))
+            {
+                Console.WriteLine($"${email} is a valid email address");
+            }
+            else Console.WriteLine($"${email} is invalid email address");
+
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp2;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /* Uncomment to run assignment 2-1*/
+
+            //Assignment_2_1 assignment_2_1 = new Assignment_2_1();
+            //assignment_2_1.assignment2_1();
+
+            /* Uncomment to run assignment 2-2*/
+
+            //Assignment_2_2 assignment_2_2 = new Assignment_2_2();
+            //assignment_2_2.assignment2_2();
+
+
+            /* Uncomment to run assignment 2-3*/
+
+            //Assignment_2_3 assignment_2_3 = new Assignment_2_3();
+            //assignment_2_3.assignment2_3();
+
+            /* Uncomment to run assignment 2-4*/
+
+            //Assignment_2_4 assignment_2_4 = new Assignment_2_4();
+            //assignment_2_4.assignment2_4();
+
+            /* Uncomment to run assignment 2-5*/
+
+            //Assignment_2_5 assignment_2_5 = new Assignment_2_5();
+            //assignment_2_5.assignment2_5();
+
+            /* Uncomment to run assignment 2-6*/
+
+            //Assignment_2_6 assignment_2_6 = new Assignment_2_6();
+            //assignment_2_6.assignment2_6();
+
+        }
+    }
+}
