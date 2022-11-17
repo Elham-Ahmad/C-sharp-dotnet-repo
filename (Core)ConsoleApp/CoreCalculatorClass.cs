3e5815ebff5df710d22e634d73ff5b2157d5f9db
Elham Ahmad <Elham.Ahmad@emtecinc.com>
2022-11-17 12:05:10 +0530

    First Commit with Assignment-1 (17/11/22)

---
+++ (Core)ConsoleApp/CoreCalculatorClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _Core_ConsoleApp
+{
+    internal class CoreCalculatorClass
+    {
+        public string Add(int num1, int num2)
+        {
+            return $"addition of {num1} and {num2} is {num1 + num2}";
+        }
+
+        public string Subtract(int num1, int num2)
+        {
+            return $"Subtraction of {num1} and {num2} is {num1 - num2}";
+        }
+        public string Multiply(int num1, int num2)
+        {
+            return $"Multiplication of {num1} and {num2} is {num1 * num2}";
+        }
+        public string Divide(int num1, int num2)
+        {
+            return $"Division  of {num1} and {num2} is {num1 / num2}";
+        }
+        public string Modulus(int num1, int num2)
+        {
+            return $"Modulus  of {num1} and {num2} is {num1 % num2}";
+        }
+    }
+}
+
---
+++ (Core)ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _Core_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new CoreCalculatorClass().Add(10, 2));
+            Console.WriteLine(new CoreCalculatorClass().Subtract(10, 2));
+            Console.WriteLine(new CoreCalculatorClass().Multiply(10, 2));
+            Console.WriteLine(new CoreCalculatorClass().Divide(10, 2));
+            Console.WriteLine(new CoreCalculatorClass().Modulus(10, 2));
+
+            Console.ReadKey();
+
+        }
+    }
+}
