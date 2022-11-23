c42349b1f36bbbb0af2bc2a815df8de779aee97e
Elham Ahmad <Elham.Ahmad@emtecinc.com>
2022-11-24 01:13:26 +0530

    First Commit with Dapper Assignment on 24/11/22

---
+++ DapperCRUD/Controllers/EmployeeController.cs
@@ -0,0 +1,70 @@
+﻿using DapperCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DapperCRUD.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepository employeeRepository;
+
+        public EmployeeController()
+        {
+            employeeRepository = new EmployeeRepository();
+        }
+
+
+
+        //Get All
+        [HttpGet]
+        [Route("Get")]
+        public IEnumerable<Employee> GetAll()
+        {
+            return employeeRepository.GetAll();
+        }
+
+        //Get By Id 
+        [HttpGet]
+        [Route("Get/{id}")]
+        public Employee GetById(int id)
+        {
+            return (Employee)employeeRepository.GetById(id);
+        }
+
+        // Insert 
+        [HttpPost]
+
+        public void Post([FromBody]Employee employee)
+        {
+            if (ModelState.IsValid)
+                employeeRepository.Add(employee);
+        }
+
+        //update 
+
+        [HttpPut("{id}")]
+
+        public void Put(int id , [FromBody] Employee employee)
+        {
+            employee.Id = id;
+
+            if (ModelState.IsValid)
+                employeeRepository.Update(employee);
+        }
+
+
+        //Delete
+
+        [HttpDelete]
+
+        public void Delete(int id)
+        {
+          
+                employeeRepository.Delete(id);
+        }
+
+    }
+}
---
+++ DapperCRUD/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DapperCRUD.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public int Designation{ get; set; }
+        public int Department { get; set; }
+
+    }
+}
